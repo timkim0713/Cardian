65dbbb90e364e10ba1fc8d042512c4f2a7333ea8
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2021-05-23 04:25:05 +0900

    m

--- application/BottomTabScreens/MyPage/MyPageScreen.js
+++ application/BottomTabScreens/MyPage/MyPageScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { ListItem, Icon, Avatar } from 'react-native-elements'
-
-import { Text, View, StyleSheet, ScrollView, Alert, RefreshControl, ActivityIndicator, Image } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  View,
+} from 'react-native';
 
 import FA5Icon from 'react-native-vector-icons/FontAwesome5';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -12,6 +21,7 @@ import MedalIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ICON_COLOR = '#FFCD4A';
 const THEME_COLOR = '#242424';
+import WATCH_IMG from '../../assets/images/watch.png';
 
 
 const list = [
@@ -31,11 +41,6 @@ const list = [
     type: 'MaterialIcons'
   },
   {
-    title: 'Galaxy Watch Connection',
-    icon: 'watch',
-    type: 'Octicons'
-  },
-  {
     title: 'SOS History',
     icon: 'history',
     type: ''
@@ -48,9 +53,9 @@ const list = [
 ]
 const PROFILE = [
   {
-    "name" : "Nahyun An",
-    "Age" : "22",
-    "Gender" : "Female",
+    "name": "Nahyun An",
+    "Age": "22",
+    "Gender": "Female",
     "avatar_url": "https://raw.githubusercontent.com/timkim0713/Cardian/master/application/assets/images/anna.png"
   }
 ]
@@ -88,7 +93,33 @@ export default function MyPageScreen() {
           ))
         }
       </View>
+      <View style={{alignItems:'center'}}>
+        <ImageBackground source={WATCH_IMG} style={{ width: 200, height: 300 }}>
+          <View
+            style={{
+              marginTop: 83,
+              marginLeft: 44,
+              padding: 10,
+              backgroundColor: 'black',
+              width: 123,
+              height: 140,
+              borderRadius: 20,
+            }}>
+            <Text style={{ color: 'red', marginBottom: 8, fontSize: 12 }}>
+              Cardian Activated
+            </Text>
+            <Text style={{ color: 'white', fontSize: 12 }}>❤️BPM: 0</Text>
+            <Text style={{ color: 'white', fontSize: 12 }}>🩸Blood Type:AB</Text>
+            <Text style={{ color: 'white', fontSize: 12 }}>Guardian Contact</Text>
+            <Text style={{ color: 'white', fontSize: 12 }}> 010-2673-0623</Text>
 
+            <Text style={{ color: ICON_COLOR, marginTop: 10 }}>
+              "I am Pregnant"
+            </Text>
+          </View>
+        </ImageBackground>
+
+      </View>
     </View>
   );
 }
