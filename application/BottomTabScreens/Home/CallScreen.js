d749b09167126ea1ba5243ca956e8c0e4d19a5c5
Daekyung Kim <timkim0713@gmail.com>
2021-05-23 02:19:30 +0900

    update

--- application/BottomTabScreens/Home/CallScreen.js
+++ application/BottomTabScreens/Home/CallScreen.js
@@ -17,7 +17,7 @@ import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 
-import WATCH_IMG from '../../assets/images/watch.png';
+import CONNECTION_IMG from '../../assets/images/connection.gif';
 
 import CircularComponent from './CircularComponent';
 
@@ -25,6 +25,8 @@ const THEME_COLOR = 'salmon';
 
 export default function CallScreen() {
   const [buttonComplete, setButtonComplete] = React.useState('SOS');
+  const [buttonColor, setButtonColor] = React.useState('black');
+  const [complete, setComplete] = React.useState(false);
 
   return (
     <View
@@ -39,30 +41,24 @@ export default function CallScreen() {
         style={{
           padding: 40,
         }}>
-        <Text
-          style={{
-            marginTop: 55,
-            marginBottom: 20,
-            padding: 0,
-            backgroundColor: 'white',
-            fontWeight: '800',
-            fontSize: 22,
-            letterSpacing: 2,
-          }}>
-          " Cardian is ready to look for nearby vehicles to help you out in case
-          you are in any kind of danger. Do not worry, we will find you and help
-          you! "
-        </Text>
-        {/* <CircularComponent /> */}
-        {/* <Text
-          style={{
-            backgroundColor: 'white',
-            fontWeight: '800',
-            fontSize: 20,
-            letterSpacing: 1,
-          }}>
-          Do not worry, we will find you and help you! "
-        </Text> */}
+        {complete ? (
+          <Image source={CONNECTION_IMG} style={{width: 250}}></Image>
+        ) : (
+          <Text
+            style={{
+              marginTop: 55,
+              marginBottom: 20,
+              padding: 0,
+              backgroundColor: 'white',
+              fontWeight: '800',
+              fontSize: 22,
+              letterSpacing: 2,
+            }}>
+            " Cardian is ready to look for nearby vehicles to help you out in
+            case you are in any kind of danger. Do not worry, we will find you
+            and help you! "
+          </Text>
+        )}
       </View>
       <View
         style={{
@@ -71,47 +67,25 @@ export default function CallScreen() {
           marginBottom: '35%',
         }}>
         <FillUpButton
-          increment={0.01}
-          buttonBackgroundColor={'black'}
+          increment={0.02}
+          buttonBackgroundColor={buttonColor}
           fillupColor={'salmon'}
-          height={150}
-          width={300}
+          height={complete ? 100 : 140}
+          width={complete ? 300 : 280}
           buttonText={buttonComplete}
           incrementSpeed={10}
-          buttonTextStyle={{fontSize: 20, color: 'white', fontWeight: 'bold'}}
-          activeOpacity={0.7}
+          buttonTextStyle={
+            buttonComplete == 'SOS'
+              ? {fontSize: 34, color: 'white', fontWeight: 'bold'}
+              : {fontSize: 20, color: 'white', fontWeight: 'bold'}
+          }
+          activeOpacity={0.8}
           onFilled={() => {
-            setButtonComplete('REQUEST SENT TO NEARBY VEHICLES');
+            setButtonComplete('FINDING NEARBY VEHICLE!');
+            setButtonColor('salmon');
+            setComplete(true);
           }}
         />
-        {/* <TouchableOpacity>
-          <View
-            style={{
-              borderRadius: 100,
-              padding: 20,
-              backgroundColor: 'white',
-              borderColor: 'black',
-              borderWidth: 5,
-              fontWeight: '800',
-              fontSize: 16,
-              width: 200,
-              height: 200,
-            }}>
-            <Text
-              style={{
-                marginTop: 55,
-                textAlign: 'center',
-                padding: 0,
-                backgroundColor: 'white',
-                fontWeight: '800',
-                fontSize: 40,
-                letterSpacing: 10,
-                fontWeight: 'bold',
-              }}>
-              SOS
-            </Text>
-          </View>
-        </TouchableOpacity> */}
       </View>
     </View>
   );
