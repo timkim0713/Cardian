ac36824d6a0972cad118e860f376c076a0c4b394
Daekyung Kim <timkim0713@gmail.com>
2021-05-23 02:47:50 +0900

    'udpate

--- application/BottomTabScreens/Home/CallScreen.js
+++ application/BottomTabScreens/Home/CallScreen.js
@@ -18,6 +18,7 @@ import FAIcon from 'react-native-vector-icons/FontAwesome';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 
 import CONNECTION_IMG from '../../assets/images/connection.gif';
+import BREATHING_IMG from '../../assets/images/breathing.gif';
 
 import CircularComponent from './CircularComponent';
 
@@ -35,14 +36,14 @@ export default function CallScreen() {
 
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'white',
+        backgroundColor: complete ? 'rgb(241,242,243)' : 'white',
       }}>
       <View
         style={{
           padding: 40,
         }}>
         {complete ? (
-          <Image source={CONNECTION_IMG} style={{width: 250}}></Image>
+          <></>
         ) : (
           <Text
             style={{
@@ -64,28 +65,61 @@ export default function CallScreen() {
         style={{
           flex: 1,
           justifyContent: 'flex-end',
-          marginBottom: '35%',
+          marginBottom: '50%',
         }}>
-        <FillUpButton
-          increment={0.02}
-          buttonBackgroundColor={buttonColor}
-          fillupColor={'salmon'}
-          height={complete ? 100 : 140}
-          width={complete ? 300 : 280}
-          buttonText={buttonComplete}
-          incrementSpeed={10}
-          buttonTextStyle={
-            buttonComplete == 'SOS'
-              ? {fontSize: 34, color: 'white', fontWeight: 'bold'}
-              : {fontSize: 20, color: 'white', fontWeight: 'bold'}
-          }
-          activeOpacity={0.8}
-          onFilled={() => {
-            setButtonComplete('FINDING NEARBY VEHICLE!');
-            setButtonColor('salmon');
-            setComplete(true);
-          }}
-        />
+        {complete ? (
+          <Image
+            source={BREATHING_IMG}
+            style={{
+              position: 'absolute',
+              width: 400,
+              height: 400,
+              marginBottom: 0,
+              alignContent: 'center',
+            }}></Image>
+        ) : (
+          <></>
+        )}
+
+        {complete ? (
+          <View
+            style={{
+              justifyContent: 'center',
+              alignContent: 'center',
+              padding: 125,
+              borderRadius: 300,
+              backgroundColor: 'salmon',
+            }}></View>
+        ) : (
+          <FillUpButton
+            increment={0.02}
+            buttonBackgroundColor={buttonColor}
+            fillupColor={'salmon'}
+            height={140}
+            width={280}
+            buttonText={buttonComplete}
+            incrementSpeed={10}
+            buttonTextStyle={
+              buttonComplete == 'SOS'
+                ? {fontSize: 34, color: 'white', fontWeight: 'bold'}
+                : {fontSize: 20, color: 'white', fontWeight: 'bold'}
+            }
+            activeOpacity={0.8}
+            onFilled={() => {
+              setButtonComplete('');
+              setButtonColor('salmon');
+              setComplete(true);
+            }}
+          />
+        )}
+        {complete ? (
+          <Text style={{width: 250, color: 'grey', marginTop: 15}}>
+            * Emergency request has been sent to nearby vehicles. Please wait
+            until Cardians arrive.
+          </Text>
+        ) : (
+          <></>
+        )}
       </View>
     </View>
   );
